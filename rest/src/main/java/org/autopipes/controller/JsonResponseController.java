cc8432b2e3884545b98672baa84454977124c480
jhrabows <janh@norwoodlight.com>
2021-02-13 18:19:40 -0500

    storage service and get-all-drawings method

--- rest/src/main/java/org/autopipes/controller/JsonResponseController.java
+++ rest/src/main/java/org/autopipes/controller/JsonResponseController.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.autopipes.model.Employee;
+import org.autopipes.model.FloorDrawing;
+import org.autopipes.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +16,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/json")
 public class JsonResponseController {
+	
+	@Autowired
+	@Qualifier("storageService")
+	private StorageService storageService;
 
+	@RequestMapping(value = "/drawing", method = RequestMethod.GET) //, produces = "application/json")
+	public @ResponseBody List<FloorDrawing> getAllDrawings() {
+		return storageService.findAllDrawings();
+	}
+	
 	@RequestMapping(value = "/employees", method = RequestMethod.GET) //, produces = "application/json")
 	public @ResponseBody List<Employee> getAllEmployee() {
 
@@ -29,4 +42,15 @@ public class JsonResponseController {
 
 		return employees;
 	}
+
+
+	public StorageService getStorageService() {
+		return storageService;
+	}
+
+
+	public void setStorageService(StorageService storageService) {
+		this.storageService = storageService;
+	}
+	
 }
